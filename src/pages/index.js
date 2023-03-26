febe11a28b60f261a4cf9d46659acdf75dda19f5
chahineBoude <chahineboude@gmail.com>
2023-03-26 13:06:13 +0100

    i guess final

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
 import AboutUs from "@/components/containers/AboutUs";
 import FindUs from "@/components/containers/FindUs";
-import Footer from "@/components/containers/Footer";
 import Header from "@/components/containers/Header";
-import Pics from "@/components/containers/Pics";
 import SpecialMenu from "@/components/containers/SpecialMenu";
 import Layout from "@/components/Layout";
 
