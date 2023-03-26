7f01619f4c7ecde2c6ac76e64eb53977f9e730b3
chahineBoude <chahineboude@gmail.com>
2023-03-26 13:32:48 +0100

    adding header

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,14 @@ import FindUs from "@/components/containers/FindUs";
 import Header from "@/components/containers/Header";
 import SpecialMenu from "@/components/containers/SpecialMenu";
 import Layout from "@/components/Layout";
+import Head from "next/head";
 
 const App = () => (
   <div>
+    <Head>
+      <title>Kaiju Restaurant</title>
+      <meta property="og:title" content="Kaiju Restaurant" key="title" />
+    </Head>
     <>
       <Layout>
         <Header />
