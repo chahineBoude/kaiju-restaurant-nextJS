bb2a23495c58292fe6cc1cf4fd4da23e80f981df
chahineBoude <chahineboude@gmail.com>
2023-03-27 10:49:14 +0100

    Update menu.js

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -3,6 +3,10 @@ import Layout from "@/components/Layout";
 
 const Menu = () => (
   <div>
+    <Head>
+      <title>Kaiju Restaurant</title>
+      <meta property="og:title" content="Kaiju Restaurant" key="title" />
+    </Head>
     <Layout>
       <MainMenu />
     </Layout>
