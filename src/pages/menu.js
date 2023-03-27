73c26034221ee9f50da3b845fc407c8c70ca3159
chahineBoude <chahineboude@gmail.com>
2023-03-27 11:07:22 +0100

    Update menu.js

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,11 +1,12 @@
 import MainMenu from "@/components/containers/MainMenu";
 import Layout from "@/components/Layout";
+import Head from "next/head";
 
 const Menu = () => (
   <div>
     <Head>
-      <title>Menu</title>
-      <meta property="og:menu" content="Menu Page" key="menu" />
+      <title>Kaiju Restaurant</title>
+      <meta property="og:title" content="Kaiju Restaurant" key="title" />
     </Head>
     <Layout>
       <MainMenu />
